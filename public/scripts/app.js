8a4c6a44c136dc12168f32cd356b9a60d7dbed05
Chris Morgan <chris.j.r.morgan@gmail.com>
2015-06-09 22:20:22 +1200

    semi-finalised frontend and added gitignore

---
+++ public/scripts/app.js
@@ -0,0 +1,82 @@
+/**
+ * Created by chris on 16/05/15.
+ */
+/* global angular */
+
+var hipsterTodo = angular.module('hipsterTodo', []);
+
+function mainController($scope, $http) {
+  $scope.newItemName = '';
+  $scope.todos = [
+    {
+      id: 1,
+      name: 'Buy potatoes',
+      completed: true,
+      datetimeCreated: 1431737332288,
+      datetimeCompleted: 1431737371434
+    },
+    {
+      id:2,
+      name: 'Create hipster web tutorial',
+      completed: false,
+      datetimeCreated: 1431737359923
+    },
+    {
+      id:3,
+      name: 'Drink beer',
+      completed: false,
+      datetimeCreated: 1431737372434
+    }
+  ];
+
+  $scope.createTodo = function () {
+    var newTodo = {
+      name: $scope.newItemName,
+      completed: false,
+      datetimeCreated: Date.now()
+    };
+
+    $scope.todos.push(newTodo);
+
+    console.log('New todo: ', newTodo);
+    $scope.newItemName = '';
+
+    console.log('New todo: ', newTodo);
+
+    $http.post('/api/todo', newTodo)
+      .success(function (data) {
+        console.log('Todo saved successfully: ', data);
+        newTodo.id = data.id
+      })
+      .error(function (data) {
+        console.error('Failed to save todo: ', data)
+      })
+  };
+  
+  $scope.checkTodo = function (todo) {
+    todo.datetimeCompleted = Date.now();
+    todo.completed = true;
+
+    $http.put('/api/todo/' + todo.id, todo)
+      .success(function (data) {
+        console.log('Todo saved successfully: ', data);
+      })
+      .error(function (data) {
+        console.error('Failed to save todo: ', data);
+      });
+  };
+
+  $scope.uncheckTodo = function (todo) {
+    todo.completed = false;
+    delete todo.datetimeCompleted;
+
+    $http.put('/api/todo/' + todo.id, todo)
+      .success(function (data) {
+        console.log('Todo saved successfully: ', data);
+      })
+      .error(function (data) {
+        console.error('Failed to save todo: ', data);
+      });
+  }
+
+}
